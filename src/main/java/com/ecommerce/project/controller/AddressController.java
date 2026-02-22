ed831a19ad423b9038274387bfbfab9dfec57ef0
Paul <badeapaul3@gmail.com>
2026-02-22 20:59:10 +0200

    Add various methods to interact with user addresses

--- src/main/java/com/ecommerce/project/controller/AddressController.java
+++ src/main/java/com/ecommerce/project/controller/AddressController.java
@@ -9,10 +9,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -29,10 +28,38 @@ public class AddressController {
         User user = authUtil.loggedInUser();
         AddressDTO savedAddressDTO = addressService.createAddress(addressDTO, user);
         return new  ResponseEntity<>(savedAddressDTO,HttpStatus.CREATED);
-
     }
 
+    @GetMapping("/addresses")
+    public ResponseEntity<List<AddressDTO>> getAddresses() {
+        List<AddressDTO> addressList = addressService.getAddresses();
+        return new ResponseEntity<>(addressList,HttpStatus.OK);
+    }
 
+    @GetMapping("/addresses/{addressId}")
+    public ResponseEntity<AddressDTO> getAddressById(@PathVariable Long addressId) {
+        AddressDTO addressDTO = addressService.getAddressById(addressId);
+        return new ResponseEntity<>(addressDTO,HttpStatus.OK);
+    }
 
+    @GetMapping("/users/addresses")
+    public ResponseEntity<List<AddressDTO>> getUserAddresses() {
+        User user = authUtil.loggedInUser();
+        List<AddressDTO> addressList = addressService.getUserAddresses(user);
+        return new ResponseEntity<>(addressList,HttpStatus.OK);
+    }
+
+    @PutMapping("/addresses/{addressId}")
+    public ResponseEntity<AddressDTO> updateAddressById(@PathVariable Long addressId,
+                                                        @RequestBody AddressDTO addressDTO) {
+        AddressDTO updatedAddressDTO = addressService.updateAddress(addressId, addressDTO);
+        return new ResponseEntity<>(updatedAddressDTO,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/addresses/{addressId}")
+    public ResponseEntity<String> deleteAddressById(@PathVariable Long addressId) {
+        String status = addressService.deleteAddressById(addressId);
+        return new ResponseEntity<>(status,HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
@@ -1,13 +1,14 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Address;
 import com.ecommerce.project.model.User;
 import com.ecommerce.project.payload.AddressDTO;
 import com.ecommerce.project.repositories.AddressRepository;
+import com.ecommerce.project.repositories.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
 import java.util.List;
 
@@ -19,6 +20,9 @@ public class AddressServiceImpl implements AddressService {
 
     @Autowired
     private AddressRepository addressRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public AddressDTO createAddress(AddressDTO addressDTO, User user) {
@@ -32,4 +36,66 @@ public class AddressServiceImpl implements AddressService {
 
         return modelMapper.map(savedAddress,AddressDTO.class);
     }
+
+    @Override
+    public List<AddressDTO> getAddresses() {
+        List<Address> addresses = addressRepository.findAll();
+        List<AddressDTO> addressDTOList = addresses.stream().map(
+                address -> modelMapper.map(address,AddressDTO.class))
+                .toList();
+        return addressDTOList;
+    }
+
+    @Override
+    public AddressDTO getAddressById(Long addressId) {
+        Address address = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address", "addressId", addressId));
+
+        AddressDTO addressDTO = modelMapper.map(address,AddressDTO.class);
+        return addressDTO;
+    }
+
+    @Override
+    public List<AddressDTO> getUserAddresses(User user) {
+        List<Address> addresses = user.getAddresses();
+        List<AddressDTO> addressDTOList = addresses.stream().map(
+                        address -> modelMapper.map(address,AddressDTO.class))
+                .toList();
+        return addressDTOList;
+    }
+
+    @Override
+    public AddressDTO updateAddress(Long addressId, AddressDTO  addressDTO) {
+        Address addressFromDatabase = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address", "addressId", addressId));
+        addressFromDatabase.setCity(addressDTO.getCity());
+        addressFromDatabase.setPincode(addressDTO.getPincode());
+        addressFromDatabase.setState(addressDTO.getState());
+        addressFromDatabase.setCountry(addressDTO.getCountry());
+        addressFromDatabase.setStreet(addressDTO.getStreet());
+        addressFromDatabase.setBuildingName(addressDTO.getBuildingName());
+
+        Address updatedAddress = addressRepository.save(addressFromDatabase);
+        User user = addressFromDatabase.getUser();
+        //remove the address from user's addresses list and add the new one
+        user.getAddresses().removeIf(address -> address.getAddressId().equals(addressId));
+        user.getAddresses().add(updatedAddress);
+        userRepository.save(user);
+
+        return modelMapper.map(updatedAddress,AddressDTO.class);
+    }
+
+    @Override
+    public String deleteAddressById(Long addressId) {
+        Address addressFromDatabase = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address", "addressId", addressId));
+        User user = addressFromDatabase.getUser();
+
+        user.getAddresses().removeIf(address -> address.getAddressId().equals(addressId));
+        userRepository.save(user);
+        addressRepository.delete(addressFromDatabase);
+        return "Address deleted";
+    }
+
+
 }
